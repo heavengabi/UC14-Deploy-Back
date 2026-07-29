640baa22c5e7aa4b892975d4f0a487f9da4e88db
Gabi <heavenxg89@gmail.com>
2026-07-29 16:04:55 -0300

    feat:aaa

--- src/config/dataSource.ts
+++ src/config/dataSource.ts
@@ -1,27 +1,28 @@
-import 'reflect-metadata'
-import dotenv from 'dotenv'
-import { DataSource } from 'typeorm';
+import "reflect-metadata";
+import dotenv from "dotenv";
+import { DataSource } from "typeorm";
 
-dotenv.config()
+dotenv.config();
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME } = process.env
+const { DB_HOST, DB_PORT, DB_USER, DB_PWD, DB_NAME } = process.env;
 
 export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: DB_HOST,
-    port: Number(DB_PORT),
-    username: DB_USER,
-    password: DB_PWD,
-    database: DB_NAME,
-    entities: ['src/models/*.ts'],
-    synchronize: false,
-    logging: true,
-    ssl:false
-})
+  type: "mysql",
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USER,
+  password: DB_PWD,
+  database: DB_NAME,
+  entities: [__dirname + "/../models/*.{js,ts}"],
+  synchronize: false,
+  logging: true,
+  ssl: false,
+});
 
 AppDataSource.initialize()
-    .then(() => {
-        console.log('Banco de dados conectado com sucesso')
-    }).catch((error) => {
-        console.error('Falha ao conectar com o banco de dados')
-    })
+  .then(() => {
+    console.log("Banco de dados conectado com sucesso");
+  })
+  .catch((error) => {
+    console.error("Falha ao conectar com o banco de dados");
+  });
