90d42831a7726ab0177496e223ef9047d9d4ea42
Gabriela <heavenxg89@gmail.com>
2026-07-29 17:00:30 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const PORT = Number(process.env.PORT || '3000')
 
 // Configurar CORS para aceitar requisições do frontend
 app.use(cors({
-  origin: 'http://localhost:', // URL do seu frontend
+  origin: 'uc-14-deploy-front.vercel.app', // URL do seu frontend
   credentials: true, // Permite enviar cookies
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
